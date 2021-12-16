401ec69181f1a505cb5fb0bf4a79d6e7b56e840a
Marco Amerotti <marco@sam.lan>
2021-12-16 11:06:56 +0100

    bug fixes

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ global.startDate = null;
 const path = require('path') ;
 const express = require('express') ;
 const cors = require("cors");
-const helmet = require("helmet");
 
 let app = express();
-//let app = svelte();
 
 
-app.use('/backoffice/js' , express.static(global.rootDir +'/src/backoffice'));
 app.use('/frontoffice/js' , express.static(global.rootDir +'/src/frontoffice'));
 app.use('/frontoffice/css' , express.static(global.rootDir +'/src/frontoffice'));
+
+app.use('/js' , express.static(global.rootDir +'/public/js'));
 app.use('/css' , express.static(global.rootDir +'/public/css'));
 app.use('/data', express.static(global.rootDir +'/public/data'));
 app.use('/docs', express.static(global.rootDir +'/public/html'));
@@ -20,7 +19,6 @@ app.use('/img' , express.static(global.rootDir +'/public/media'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.use(helmet());
 app.enable('trust proxy');
 
 app.get('/',  function (req, res) { 
