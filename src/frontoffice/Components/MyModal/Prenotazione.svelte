dacda16d90e74f136295be35c04b2bef58164069
agata cavigioli <agata.cavigioli@fastwebnet.it>
2022-03-01 22:40:58 +0100

    fix3

--- src/frontoffice/Components/MyModal/Prenotazione.svelte
+++ src/frontoffice/Components/MyModal/Prenotazione.svelte
@@ -159,7 +159,7 @@ async function checkAvailability(product_id, from, to){
            }
      }
 
-     console.log("checked");
+     //console.log("checked");
      return true;
 }
 
@@ -239,7 +239,7 @@ async function sendnolo(){
     const today = new Date();
     const diffTimetoday = today - date1;
     const diffDaystoday = Math.ceil(diffTimetoday / (1000 * 60 * 60 * 24)) - 1;
-    console.log('diffDaystoday'+diffDaystoday);
+    //console.log('diffDaystoday'+diffDaystoday);
     booking.status = (diffDaystoday==0) ? "Iniziato" : "Prenotato";
     //console.log(booking.status);
     booking.nolo_data.discount = filo.nolo_data.discount;
