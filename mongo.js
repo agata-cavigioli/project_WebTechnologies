8d93627be42e4fbf185aa000037ab857cdb2acff
Marco Amerotti - marco.amerotti@studio.unibo.it <marco.amerotti@labs.cs.unibo.it>
2022-03-02 01:05:26 +0100

    no non funziona lol

--- mongo.js
+++ mongo.js
@@ -5,6 +5,7 @@ const mongouri = 'mongodb://site202123:thieCah0@mongo_site202123?writeConcern=ma
 const mongo = new MongoClient(mongouri);
 
 exports.update = async function(query, update, collection, database='nolonolo') {
+	
 	let cursor;
 
 	try {
@@ -18,9 +19,11 @@ exports.update = async function(query, update, collection, database='nolonolo') 
 	} finally {
 		await mongo.close();
 	}
+	
 }
 
 exports.delete = async function(query, collection, database='nolonolo') {
+	
 	let cursor;
 
 	try {
@@ -34,9 +37,11 @@ exports.delete = async function(query, collection, database='nolonolo') {
 	} finally {
 		await mongo.close();
 	}
+	
 }
 
 exports.insert_many = async function(object, collection, database='nolonolo') {
+	
 	try {
 		await mongo.connect();
 
@@ -50,9 +55,11 @@ exports.insert_many = async function(object, collection, database='nolonolo') {
 	} finally {
 		await mongo.close();
 	}
+	
 }
 
 exports.insert_one = async function(object, collection, database='nolonolo') {
+	
 	try {
 		await mongo.connect();
 
@@ -66,10 +73,11 @@ exports.insert_one = async function(object, collection, database='nolonolo') {
 	} finally {
 		await mongo.close();
 	}
+	
 }
 
 exports.query = async function(query, options, collection, database='nolonolo') {
-
+	
 	let cursor;
 
 	try {
@@ -84,9 +92,11 @@ exports.query = async function(query, options, collection, database='nolonolo') 
 		await mongo.close();
 		return cursor;
 	}
+	
 }
 
 exports.erase_all = async function(collection, database='nolonolo') {
+	
 	try {
 		await mongo.connect();
 
@@ -99,61 +109,5 @@ exports.erase_all = async function(collection, database='nolonolo') {
 	} finally {
 		await mongo.close();
 	}
+	
 }
-
-/*
-exports.mongoInsert = async function() {
-	try {
-		await mongo.connect();
-
-		const database = mongo.db("insertDB");
-		const foods = database.collection("foods");
-
-		const res = await foods.deleteMany();
-		console.log(`${res.deletedCount} documents were inserted`);
-
-// create an array of documents to insert
-		const docs = [
-			{ name: "cake", healthy: false },
-			{ name: "lettuce", healthy: true },
-			{ name: "donut", healthy: false }
-		];
-
-// this option prevents additional documents from being inserted if one fails
-		const options = { ordered: true };
-		const result = await foods.insertMany(docs, options);
-		console.log(`${result.insertedCount} documents were inserted`);
-
-	} finally {
-		await mongo.close();
-	}
-}
-
-exports.mongoQuery = async function() {
-	try {
-		await mongo.connect();
-		const database = mongo.db("insertDB");
-		const foods = database.collection("foods");
-
-// query for movies that have a runtime less than 15 minutes
-		const query = { healthy: false };
-		const options = {
-			// sort returned documents in ascending order by title (A->Z)
-			sort: { name: 1 },
-		};
-
-		const cursor = foods.find(query, options);
-
-		// print a message if no documents were found
-		if ((await cursor.count()) === 0) {
-			console.log("No documents found!");
-		}
-
-		// replace console.dir with your callback to access individual elements
-		await cursor.forEach(console.dir);
-
-	} finally {
-		await mongo.close();
-	}
-}
-*/
